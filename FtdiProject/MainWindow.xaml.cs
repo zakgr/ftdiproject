43eec14fe2a2a085a156ffd86de74d042ad87861
zakgr <zakcorepower@hotmail.com>
2016-12-15 18:20:40 +0200

    ftdi added

--- FtdiProject/MainWindow.xaml.cs
+++ FtdiProject/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FTD2XX_NET;
 
 namespace FtdiProject
 {
@@ -23,6 +26,45 @@ namespace FtdiProject
         public MainWindow()
         {
             InitializeComponent();
+            
+
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //string[] ports = SerialPort.GetPortNames();
+
+        }
+
+        private void CmbPorts_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            FTDI ftdi = new FTDI();
+            FTDI.FT_DEVICE_INFO_NODE[] devicelist = new FTDI.FT_DEVICE_INFO_NODE[100];
+            ftdi.GetDeviceList(devicelist);
+            List<object> deviList = new List<object>();
+            foreach (var deviceInfo in devicelist)
+            {
+                try
+                {
+                    deviList.Add(deviceInfo.Description);
+                }
+                catch (Exception){}
+                
+            }
+            var comboBox = sender as ComboBox;
+            if (comboBox != null)
+            {
+                comboBox.ItemsSource = deviList;
+                comboBox.SelectedIndex = 0;
+            }
+
+        }
+
+        private void CmbPorts_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            string value = comboBox.SelectedItem as string;
+            this.Title = "Selected: " + value; 
         }
     }
 }
